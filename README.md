6ff495594e8a3bf5c1d9f7a2ee943aac147f178d
alu0100786330 <alu0100786330@ull.edu.es>
2016-11-09 17:46:14 +0000

    iniciando repoo

---
+++ README.md
@@ -0,0 +1,78 @@
+#Práctica: GitHub API
+
+## Descripción
+
+El objetivo es facilitar el uso del repositorio a todo tipo de usuarios, ya estén familizarizados con gitbook y github o no. Para ello haremos
+uso de la API de GitHub para poder crearle un repo en la plataforma.
+
+
+
+## Pasos a ejecutar 
+
+**1. Instala nuestro paquete de forma global**
+
+```npm install -g gitbook-start-elt```
+
+**2. Instala el plugin forma global**
+
+```npm install -g PLUGIN```
+
+**3. Ejecuta el binario para el render del template**
+
+```gitbook-start --dir Carpeta``` !!Si no ejecutas el --dir se creará una carpeta con tu nombre de usuario
+
+**4. Entra en la carpeta**
+
+ ```cd Carpeta```
+
+**5. Ejecuta el plugin que desees**
+
+```gitbook-start -d PLUGIN``` !! También puedes usar la opción --deploy
+
+
+**6. Ejecuta un npm install**
+
+```npm install```
+
+
+**7. Ejecuta el gulp creado**
+
+
+```gulp deploy-plugin```
+
+## Explicación
+
+Cunado se ejecuta el gitbook-start -d PLUGIN se te lanzará el initialize del módulo,
+el initialize crea una tarea en el gulp para realizar el deploy. Además de guardarte el paquete
+elegido en el package.json.
+
+## Opciones
+
+    gitbook-start [OPTIONS]
+        -d nombre del directorio a crear node gitbook-star -d miDirectorio
+        -a autor del libro a crear node gitbook-star -a AutorDelLibro
+        -e email del autor del libro node gitbook-star -e eric.ramos.suarez@gmail.com
+        -r repositorio github contra el que se va a trabajar -r nameRepo
+        -v muestra la version del paquete gitbook-start -v
+        -h muestra ayuda sobre las opciones disponibles
+
+
+## Enlaces interesantes 
+ 
+* [NPM](https://www.npmjs.com/package/gitbook-start-elt)
+* [Repositorio de la práctica](https://github.com/ULL-ESIT-SYTW-1617/crear-repositorio-en-github-ericlucastania-1.git)
+* [Descripción de la tarea campus](https://casianorodriguezleon.gitbooks.io/ull-esit-1617/content/practicas/practicagithubapi.html)
+* [PLUGIN HEROKU](https://github.com/ULL-ESIT-SYTW-1617/gitbook-start-heroku-ericlucastania.git)
+* [NPM PLUGIN HEROKU](https://www.npmjs.com/package/gitbook-start-plugin-heroku-ericlucastania)
+* [PLUGIN IAAS](https://github.com/ULL-ESIT-SYTW-1617/gitbook-start-heroku-ericlucastania.git)
+* [NPM PLUGIN IAAS](https://www.npmjs.com/package/gitbook-start-plugin-heroku-ericlucastania)
+
+
+
+## Componentes del grupo de trabajo
+
+* [Eric Ramos](https://github.com/alu0100786330)
+* [Lucas Ruiz](https://github.com/alu0100785265)
+* [Tania González](https://github.com/tania77)
+
+
---
+++ bin/octonode.js
@@ -0,0 +1,94 @@
+'use strict';
+
+module.exports = {     
+ 
+ 
+	octoIni: () => {
+		 return new Promise((resolviendo,reject) => {
+			 var fs = require('fs-extra');
+			 var github = require('octonode'); 
+			 var readlineSync = require('readline-sync');
+			 var username = readlineSync.question('Introduzca su nombre de usuario en Github: ');
+			 var password = readlineSync.question('Introduzca su contraseña en Github: ', {
+			 	hideEchoBack: true
+			 });
+			 
+			 var json = {
+				"token": "",
+				"id": "",
+				"user":{
+					"repo": "",
+					"email": "",
+					"name": ""
+				}
+			 };
+			function auth(){
+				return new Promise((resolve,reject) => {
+					github.auth.config({ username, password }).login({
+					  scopes: ['user', 'repo'],
+					  note: 'Token para Gitbook'
+					}, 
+					(err, id, token) => {
+					  if (err) return err;
+					  resolve(json.token = token);
+					  json.id = id;
+					 
+					});
+				});
+			} 
+			
+			
+			var directoriomonito = process.env.HOME;
+			
+			try{
+				auth().then(function(resolve,reject){
+					fs.mkdirSync(directoriomonito + '/.gitbook-start');
+					var pac = directoriomonito + '/.gitbook-start/';
+					fs.writeFile(pac + 'config.json',JSON.stringify(json), function(err){
+						if (err) throw err;
+						else resolviendo(console.log("guardando el json correctamente.."));
+						
+					});
+				});
+				
+			}
+			catch(err){
+				resolviendo(console.log("leyendo directorio..."));	
+			}
+			
+		});
+		
+              
+	},
+	
+	octoRepo: () => {
+		return new Promise((resolve,reject) => {
+			require('shelljs/global');
+			var github = require('octonode');
+			var readlineSync = require('readline-sync');
+			var fs = require('fs-extra');
+			var configJson = require(process.env.HOME + '/.gitbook-start/config.json');
+			var client = github.client(configJson.token);
+			var directorioUsuario = process.cwd() + '/';
+			var pck = require(directorioUsuario + 'package.json');
+			var dir = readlineSync.question('Introduzca su nombre del repositorio a crear en Github: ');
+			var ghme = client.me();
+			
+			ghme.repo({
+			  "name": dir,
+			  "description": "This is your Gitbook-Start repository",
+			}, (err, status, body, headers) => {
+				if (err) throw err;
+				pck.repository.url = status.ssh_url;
+				
+				fs.writeFile(directorioUsuario + 'package.json', JSON.stringify(pck));
+				resolve(exec('git remote add origin ' + status.ssh_url + ' ;git add .;git commit -m "inicializando repo";git push'));
+
+			}); //repo
+		});
+		
+	}
+	
+	
+	
+};
